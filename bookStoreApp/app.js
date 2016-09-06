380182faf6eea443a6d8ede3a14d50ff15a20a01
Raghu D <drv.work@gmail.com>
2016-09-06 16:09:51 +0530

    added states using ui-router

--- bookStoreApp/app.js
+++ bookStoreApp/app.js
@@ -1,6 +1,26 @@
 //define angular module
 (function(){
 	'use strict'
-     angular.module('myApp', []);     
+     var app = angular.module('myApp', ['ui.router']);   
+
+     app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+
+     	$urlRouterProvider.otherwise("bookList")
+
+     	$stateProvider
+                        .state('bookList', {
+                            url: "/bookList",
+                            templateUrl: "bookList.html",
+                            controller : 'addBookCtrl'
+                        })
+
+                        .state('addBook', {
+                            url: "/addBook",
+                            templateUrl: "addBook.html"
+                        })
+
+     }]);
+
+
 }
 )();
