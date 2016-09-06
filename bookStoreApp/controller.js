74e29c2346e285b58c86e662b525abd0d21ac52d
Raghu D <drv.work@gmail.com>
2016-09-06 16:11:27 +0530

    added states using ui-router

--- bookStoreApp/controller.js
+++ bookStoreApp/controller.js
@@ -7,30 +7,7 @@
         $scope.books = BookService.getBookListArray();
         $scope.book = {};
         $scope.bookToOrder = BookService.getBookToOrder();
-        $scope.err = BookService.getMessage();
-
-        $scope.addBook = function(book) {
-
-            if (!$scope.contains(book, $scope.books)) {
-                var temp = {
-                    auther: book.auther,
-                    title: book.title,
-                    numPages: book.numPages,
-                    description: book.description,
-                    inStock: book.inStock,
-                    price: book.price
-                };                
-                BookService.setMessage("Book Added");
-                $scope.err = BookService.getMessage();
-                $scope.books.push(temp);
-                // emptying the object after adding the book
-                $scope.book = {};
-
-            } else {
-                BookService.setMessage("Book already contains");
-                $scope.err = BookService.getMessage();
-            }
-        }
+        $scope.bookListMsg = BookService.getMessage();
 
         $scope.removeBook = function(book) {
             var allBooks = $scope.books;
@@ -39,7 +16,7 @@
                     $scope.books.splice(i, 1);
                 }
             }
-            $scope.err = "Book removed";
+            $scope.bookListMsg = "Book removed";
         }
 
         $scope.orderBook = function(book) {
@@ -65,28 +42,7 @@
         }
 
 
-        $scope.contains = function(book, books) {
 
-            var containsBook = false;
-            //extract sub arrays from books for each property
-            var autherArry = books.map(function(books) {
-                return books.auther;
-            });
-
-            containsBook = autherArry.indexOf(book.auther) > -1
-
-            if (containsBook) {
-                var titleArry = books.map(function(books) {
-                    return books.title;
-                });
-
-                containsBook = titleArry.indexOf(book.title) > -1
-
-            }
-
-            return containsBook;
-
-        }
 
         $scope.placeOrder = function(quantity) {
             var book = BookService.getBookToOrder();
